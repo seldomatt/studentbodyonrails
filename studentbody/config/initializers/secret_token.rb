b71a14e7dfaf9ee6bc6b2e5cf0e15649ac08050c
Matthew Salerno <matthewsalern@gmail.com>
2012-10-30 17:42:27 -0400

    initial commit

---
+++ studentbody/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentbody::Application.config.secret_token = '8aacedf6a2c9724c5241903a01f5dc3780f3cc07a364807b004e8e812cf06d88581056b0030cf4f4d182f4dc9b2f0aa3c5f0335d1d507b3da5285f32e72ec039'
